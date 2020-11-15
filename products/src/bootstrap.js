f0526a7f53beaec46d63461ad1f5a9c600668139
Shubham Jain <shubhamjain2908@gmail.com>
2020-11-15 20:41:15 +0530

    execution context comments

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -8,3 +8,15 @@ for (let i = 0; i < 5; i++) {
 }
 
 document.querySelector('#dev-products').innerHTML = products;
+
+// Context/Situation #1
+// We are sunning this file in development isolation
+// We are using our local index.html file 
+// Which DEFINITELY has an element with an id of 'dev-products'
+// We want to immediately render our app into the element
+
+// Context/Situation #2
+// We are sunning this file in development pr production
+// through the CONTAINER app
+// No GUARANTEE that an element with a id of 'dev-products' exist
+// We DO NOT want to immediately render our app into the element
