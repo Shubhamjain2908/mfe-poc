94ea022233db53d8159b00644f38f2f917c15145
Shubham Jain <shubhamjain2908@gmail.com>
2020-11-15 20:46:17 +0530

    refactor products

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,22 +1,34 @@
 import faker from 'faker';
 
-let products = '';
+const monut = el => {
+    let products = '';
 
-for (let i = 0; i < 5; i++) {
-    const name = faker.commerce.productName();
-    products += `<div>${name}</div>`;
-}
+    for (let i = 0; i < 5; i++) {
+        const name = faker.commerce.productName();
+        products += `<div>${name}</div>`;
+    }
 
-document.querySelector('#dev-products').innerHTML = products;
+    el.innerHTML = products;
+};
 
 // Context/Situation #1
 // We are sunning this file in development isolation
 // We are using our local index.html file 
 // Which DEFINITELY has an element with an id of 'dev-products'
 // We want to immediately render our app into the element
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector('#dev-products');
+
+    // Assuming our container doesnt have an element with id 'dev-products...'
+    if (el) {
+        // we are probably running in isolation
+        monut(el);
+    }
+}
 
 // Context/Situation #2
 // We are sunning this file in development pr production
 // through the CONTAINER app
 // No GUARANTEE that an element with a id of 'dev-products' exist
 // We DO NOT want to immediately render our app into the element
+export { monut };
